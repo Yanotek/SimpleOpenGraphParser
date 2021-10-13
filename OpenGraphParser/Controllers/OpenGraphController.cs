3b4558ca4a97d7a5b27faf2444ae7ef13e495a89
joni_no <joni_no@mail.ru>
2021-10-13 10:43:18 +0300

    Add ParseMetaTags

--- OpenGraphParser/Controllers/OpenGraphController.cs
+++ OpenGraphParser/Controllers/OpenGraphController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -15,6 +16,12 @@ namespace OpenGraphParser.Controllers
     public class OpenGraphController : ControllerBase
     {
         private readonly IMemoryCache _memoryCache;
+        
+        private static readonly List<string> NeededMetaTags = new()
+        {
+            "description",
+            "title"
+        };
 
         public OpenGraphController(IMemoryCache memoryCache)
         {
@@ -40,6 +47,11 @@ namespace OpenGraphParser.Controllers
                     result.Add(metadata.Key, metadata.Value.FirstOrDefault()?.Value ?? "");
                 }
 
+                if (!result.Any())
+                {
+                    result = await ParseMetaTags(url);
+                }
+
                 _memoryCache.Set(url, result, new MemoryCacheEntryOptions()
                 {
                     Size = 1,
@@ -52,6 +64,39 @@ namespace OpenGraphParser.Controllers
             {
                 return BadRequest($"Unhandled exception: {ex.Message}");
             }
+        }
+        
+        private async Task<Dictionary<string, string>> ParseMetaTags(string url)
+        {
+            var result = new Dictionary<string, string>();
+
+            var web = new HtmlWeb();
+            var doc = await web.LoadFromWebAsync(url);
+
+            var titleNode = doc.DocumentNode.SelectSingleNode("//title");
+            if (titleNode != null)
+            {
+                result.Add("title", titleNode.InnerText);
+            }
+
+            var nodes = doc.DocumentNode.SelectNodes("//meta");
+
+            foreach (var node in nodes)
+            {
+                var contentAttribute = node.Attributes.FirstOrDefault(x => x.Name == "content")?.Value;
+                var nameAttribute = node.Attributes.FirstOrDefault(x => x.Name == "name")?.Value?.ToLower();
+                if (nameAttribute == null)
+                {
+                    continue;
+                }
+
+                if (NeededMetaTags.Contains(nameAttribute) && !result.ContainsKey(nameAttribute))
+                {
+                    result.Add(nameAttribute, contentAttribute ?? "");
+                }
+            }
+
+            return result;
         }
     }
 }
